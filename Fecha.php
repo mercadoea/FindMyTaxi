8d253a396c411befcb20e1ac8632603679afc2ba
mcknago <74916664+mcknago@users.noreply.github.com>
2021-09-06 15:32:15 -0500

    Update Fecha.php

--- Fecha.php
+++ Fecha.php
@@ -1,6 +1,6 @@
 <?php 
 include("Conexion.php");
-$consulta = "SELECT * FROM location ORDER BY time DESC LIMIT 1";
+$consulta = "SELECT * FROM location ORDER BY ID DESC LIMIT 1";
 		$resultado = mysqli_query($connection,$consulta);
 		$row = mysqli_fetch_array($resultado);
 		$Fecha = $row['date'];
