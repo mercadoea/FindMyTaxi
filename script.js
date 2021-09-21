5e4bac7a37968b9876591f3c278da107da5c879d
mercadoea <73907682+mercadoea@users.noreply.github.com>
2021-09-21 01:38:53 -0500

    update

---
+++ script.js
@@ -0,0 +1,54 @@
+
+var Latitud = 0;
+var Longitud = 0;
+var markers = new Array;
+var position = new Array;
+var start = true;
+
+
+var map = L.map('map').setView([Latitud, Longitud], 16);
+L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>'
+}).addTo(map);
+var myMarker = L.marker([Latitud, Longitud]).addTo(map);
+var polyline = L.polyline([],{color: 'black'}).addTo(map);
+
+
+
+
+
+
+$(document).ready(function () {
+    setInterval(
+        function () {
+
+            $('#Latitud').load('Latitud.php');
+            $('#Longitud').load('Longitud.php');
+            $('#Fecha').load('Fecha.php');
+            $('#Hora').load('Hora.php');
+            var Latitud = parseFloat($('#Latitud').text());
+            var Longitud = parseFloat($('#Longitud').text());
+            //map.setView([Latitud, Longitud]);
+
+            position[0] = Latitud;
+            position[1] = Longitud;
+
+            markers.push(position);
+            if (start){
+                markery = new L.marker(markers.at(0)).addTo(map);
+                start = false;
+            }
+            
+            
+
+            var newLatLng = new L.LatLng(Latitud, Longitud);
+            myMarker.setLatLng(newLatLng);
+            polyline.addLatLng(newLatLng);
+            map.fitBounds(polyline.getBounds());
+
+
+
+
+        }, 1500
+    );
+});
