d4473fa8cb33b369f7654d6e701bad0a1a775e9d
Daniela Jaraba <mdjaraba@uninorte.edu.co>
2021-09-28 09:59:56 -0500

    Comentarios en Conexion.php

--- Conexion.php
+++ Conexion.php
@@ -1,5 +1,11 @@
 <?php
 
+/*
+Archivo de conexion a la base de datos en AWS
+Tener cuidado con instalar php-mysqli para que funcione
+Testeado con PHP 7.4
+*/
+
 $host = "findmytaxi.cbjilbug2hvk.us-east-2.rds.amazonaws.com";
 $username = "admin";
 $password = "jaraba6234";
