59a78fac9895dbebf7c0ecbc48867e6859c30c8e
mcknago <74916664+mcknago@users.noreply.github.com>
2021-09-28 01:38:02 -0500

    Update historial.php

--- historial.php
+++ historial.php
@@ -31,45 +31,62 @@
 		&nbsp;&nbsp; Historial
 		</a>
 
+<form method="POST" action="historial.php">
 		<div class="datetime">
 			<hr></hr>
 			<br>
 				<b style="font-size: 18px;">Selecciona la fecha y hora inicial de filtrado:
 					<br>				
-						<input type="datetime-local" id="timestamp" style="width: 210px;" data-date-format="DD MMMM YYYY, h:mm:ss">
+						<input type="datetime-local" name="date_i" id="timestamp" style="width: 210px;" data-date-format="DD MMMM YYYY, h:mm:ss">
 					</br>
 				</b>
 			</br>
 			<br>
 				<b style="font-size: 18px;">Selecciona la fecha y hora final de filtrado:
 					<br>				
-						<input type="datetime-local" id="timestamp" style="width: 210px;" data-date-format="DD MMMM YYYY, h:mm:ss">
+						<input type="datetime-local" name="date_f" id="timestamp" style="width: 210px;" data-date-format="DD MMMM YYYY, h:mm:ss">
 					</br>
 				</b>
 			</br>
 			<br>			
-				<button class="searchbtn" id="searchDateTime">Buscar&#128270;</button>
+				<button class="searchbtn" name="btn" id="searchDateTime">Buscar&#128270;</button>
 			</br>
 		</div>
 		
     </div>  
+</form>
+
+<?php 
+	if(isset($_POST['btn'])){
+		include("Conexion.php");
+		$Date_i=strval($_POST['date_i']);
+		$Date_f=strval($_POST['date_f']);
+		$date_i = intval(preg_replace('/[^0-9]+/', '', $Date_i), 10)."00"; 
+		$date_f = intval(preg_replace('/[^0-9]+/', '', $Date_f), 10)."00"; 
+		$historical = mysqli_query($connection,"SELECT * FROM location WHERE dateTime>=$date_i AND dateTime<=$date_f  ORDER BY ID ASC");
+		while($consulta=mysqli_fetch_array($historical)){
+			$poly[]=array((float) $consulta['lat'],(float) $consulta['lon']);
+		}		
+	}
+?>
 
 	<div id="main" class="main">
 		<button class="openbtn" id="openButton" onclick="openNav()">&#9776; Abrir Menú</button>
 		<div id="map"></div>
 
 		<script type="text/javascript">
-			var Latitud='0';
-			var Longitud='0';
-			var start = true;			
-			var map = L.map('map').setView([Latitud,Longitud],16);
+			var LatLon = [0,0];
+			var map = L.map('map').setView(LatLon,16);
 			L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 			attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>'
 			}).addTo(map);
-			var myMarker = L.marker([Latitud, Longitud]).addTo(map).bindPopup('My location')
-				.openPopup();
-			var myMarker = L.marker([Latitud, Longitud]).addTo(map);
-			var polyline = L.polyline([]).addTo(map)
+			var latlon = <?php echo json_encode($poly) ?>;
+			var inicial=latlon[0];			
+			var myMarker = L.marker(inicial).addTo(map).bindPopup('Ubicación inicial').openPopup();
+			var final=latlon[latlon.length -1];
+			var markery = L.marker(final).addTo(map).addTo(map).bindPopup('Ubicación final').openPopup();
+			var polyline = L.polyline(latlon).addTo(map);
+			map.fitBounds(polyline.getBounds())
 		</script>	
 	</div>
 </body>
@@ -88,28 +105,5 @@
 		document.getElementById("main").style.marginLeft = "0";
 		document.getElementById("openButton").style.display = "block";
 	}
-
-	$(document).ready(function(){
-		setInterval(
-				function(){
-					$('#Latitud').load('Latitud.php');
-					$('#Longitud').load('Longitud.php');
-					$('#Fecha').load('Fecha.php');
-					$('#Hora').load('Hora.php');
-					var Latitud=parseFloat($('#Latitud').text());
-					var Longitud= parseFloat($('#Longitud').text());
-					if (start){
-                	markery = new L.marker([Latitud, Longitud]).addTo(map);
-                	start = false;
-           			}
-
-					map.setView([Latitud,Longitud]);
-					var newLatLng = new L.LatLng(Latitud, Longitud);
-    					myMarker.setLatLng(newLatLng); 		
-					polyline.addLatLng([Latitud,Longitud])	
-					//map.fitBounds(polyline.getBounds());
-				},1500
-			);			
-	});
 
 </script>
