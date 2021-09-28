3f7aab924128d4338025f03c0669b9aeb4606a6f
Ykamell <90073648+Ykamell@users.noreply.github.com>
2021-09-28 10:12:29 -0500

    Update Conexion.php

--- Conexion.php
+++ Conexion.php
@@ -1,15 +1,8 @@
-<?php
-
-/*
-Archivo de conexion a la base de datos en AWS
-Tener cuidado con instalar php-mysqli para que funcione
-Testeado con PHP 7.4
-*/
-
-$host = "findmytaxi.cbjilbug2hvk.us-east-2.rds.amazonaws.com";
-$username = "admin";
-$password = "jaraba6234";
-$database = "findmytaxi";
-$connection = mysqli_connect($host, $username, $password, $database);
-
+<?php 
+$host = getenv("FINDMYTAXI_HOST");  #Si es en otro lugar va el HTTP..
+$username = getenv("FINDMYTAXI_USER");
+$password = getenv("FINDMYTAXI_PASSWORD");
+$registro = getenv("FINDMYTAXI_REG");
+$connection = mysqli_connect($host, $username, $password, $registro);
 ?>
+
