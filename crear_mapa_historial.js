f20c338f6a8f60f47ff7cdb5aad927d0661a15cf
mcknago <74916664+mcknago@users.noreply.github.com>
2021-10-03 21:55:07 -0500

    Update crear_mapa_historial.js

--- crear_mapa_historial.js
+++ crear_mapa_historial.js
@@ -1,5 +1,8 @@
-var LatLon = [0,0];
-		var map = L.map('map').setView(LatLon,16);
-		L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-		attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>'
-		}).addTo(map);
+var LatLon = [10.996863,-74.810319];
+var map = L.map('map').setView(LatLon,16);
+L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>'
+}).addTo(map);
+var myMarker = L.marker(LatLon).addTo(map)
+var markery = L.marker(LatLon).addTo(map)
+var polyline = L.polyline([]).addTo(map);	
