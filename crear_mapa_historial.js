11202d3266d8f2c3c8d4da2537e5b1aae10f10e5
mcknago <74916664+mcknago@users.noreply.github.com>
2021-10-03 20:01:31 -0500

    Create crear_mapa_historial.js

---
+++ crear_mapa_historial.js
@@ -0,0 +1,5 @@
+var LatLon = [0,0];
+		var map = L.map('map').setView(LatLon,16);
+		L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+		attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>'
+		}).addTo(map);
