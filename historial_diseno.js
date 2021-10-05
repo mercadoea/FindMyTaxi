1f79d38e51e2509a8b2201e4d5c595183ff7df77
mcknago <74916664+mcknago@users.noreply.github.com>
2021-10-05 10:46:17 -0500

    Update historial_diseno.js

--- historial_diseno.js
+++ historial_diseno.js
@@ -1,4 +1,5 @@
-    function openNav() {
+//Comentario 
+function openNav() {
         document.getElementById("mySidebar").style.width = "250px";
         document.getElementById("main").style.marginLeft = "250px";
         document.getElementById("openButton").style.display = "none";
