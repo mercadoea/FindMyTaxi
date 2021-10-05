009c8991aad4407fe8bf66953031f50336acfb44
mcknago <74916664+mcknago@users.noreply.github.com>
2021-10-04 19:53:26 -0500

    Update script_mapaHistorial.js

--- script_mapaHistorial.js
+++ script_mapaHistorial.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){	
 	enviarDatos();
 });
+var latlon=[0,0];
 
 function enviarDatos(){
 
@@ -12,15 +13,36 @@ function enviarDatos(){
 		"url": "consulta_historico.php",
 		"data": frm
 	}).done(function(LatLon){
-		var latlon = JSON.parse(LatLon);
+		latlon = JSON.parse(LatLon);
 		var inicial=new L.LatLng(latlon[0][0],latlon[0][1]);
 		myMarker.setLatLng(inicial).bindPopup('Ubicación inicial').openPopup();
 		var final=new L.LatLng(latlon[latlon.length -1][0],latlon[latlon.length -1][1]);
-		console.log(final)
 		 markery.setLatLng(final).bindPopup('Ubicación final').openPopup();
 		polyline.setLatLngs(latlon);
 		map.fitBounds(polyline.getBounds());	
+		map.on('click', onMapClick);		
 	});
 });
 
 }
+
+	function onMapClick(e) {
+		var latlon_click=e.latlng;
+		var min=50;
+		var Fecha=[];
+		var n=0;
+		for(var i = 0; i<= latlon.length-1; i++){
+
+			var distance = map.distance(latlon_click , [  latlon[i][0] , latlon[i][1]   ]);
+			console.log(distance);
+				if (distance < min) {
+      			Fecha[n]=latlon[i][2];
+      			n=n+1;
+    			}
+		}
+			popup
+			.setLatLng(e.latlng)
+			.setContent("Coordenadas geográficas: <br>" + latlon_click.toString()+'<br> Fechas: <br>' + Fecha.join('<br>') )
+			.openOn(map);
+}
+
