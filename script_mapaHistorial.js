b32faeab661fd219d0d2de92d586db78299c32f4
mcknago <74916664+mcknago@users.noreply.github.com>
2021-10-05 10:00:19 -0500

    Update script_mapaHistorial.js

--- script_mapaHistorial.js
+++ script_mapaHistorial.js
@@ -2,7 +2,6 @@ function onMapClick(e) {
 	var checkBox = document.getElementById("miCheckUbication");
 	if (checkBox.checked == true){
 		var latlon_click=e.latlng;
-		var min=15;
 		var Fecha=[];
 		var n=0;
 		for(var i = 0; i<= latlon.length-1; i++){
@@ -12,6 +11,8 @@ function onMapClick(e) {
 				n=n+1;
 			}
 		}
+		circle
+		.setLatLng(latlon_click);
 		popup
 		.setLatLng(e.latlng)
 		.setContent("Coordenadas geográficas: <br>" + latlon_click.toString()+'<br> Fechas: <br>' + Fecha.join('<br>') )
