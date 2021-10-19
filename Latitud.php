3ef006473f82f66b5b834d33143ac8c4215a18b3
mcknago <74916664+mcknago@users.noreply.github.com>
2021-10-19 00:57:50 -0500

    Delete Latitud.php

--- Latitud.php
+++
@@ -1,8 +0,0 @@
-<?php 
-include("Conexion.php");
-$consulta = "SELECT * FROM location ORDER BY ID DESC LIMIT 1";
-		$resultado = mysqli_query($connection,$consulta);
-		$row = mysqli_fetch_array($resultado);
-		$Latitud = $row['lat'];
-		echo  $Latitud
-?>
