37e97f8cd051d77c3d53a4a02d8372adad310524
mcknago <74916664+mcknago@users.noreply.github.com>
2021-10-19 00:57:45 -0500

    Delete Hora.php

--- Hora.php
+++
@@ -1,8 +0,0 @@
-<?php 
-include("Conexion.php");
-$consulta = "SELECT * FROM location ORDER BY ID DESC LIMIT 1";
-		$resultado = mysqli_query($connection,$consulta);
-		$row = mysqli_fetch_array($resultado);
-		$Time = explode( ' ', $row['dateTime']);
-		echo $Time[1];
-?>
