40727bcdc342b92298eb2f5335e85847b5c614a7
Ykamell <90073648+Ykamell@users.noreply.github.com>
2021-11-14 21:51:17 -0500

    Update script_mapaHistorial.js

--- script_mapaHistorial.js
+++ script_mapaHistorial.js
@@ -1,6 +1,7 @@
 function onMapClick(e) {
 	var checkBox = document.getElementById("miCheckUbication");
 	if (checkBox.checked == true){
+		document.getElementById("historialtitle").style.display = "block";
 		var latlon_click=e.latlng;
 		var Fecha=[];
 		var n=0;
@@ -11,6 +12,7 @@ function onMapClick(e) {
 				n=n+1;
 			}
 		}
+		document.getElementById('fechashist').innerHTML = "Coordenadas geográficas: <br>" + latlon_click.toString()+'<br> Fechas: <br>' + Fecha.join('<br>');
 		circle
 		.setLatLng(latlon_click);
 		popup
@@ -18,6 +20,7 @@ function onMapClick(e) {
 		.setContent("Coordenadas geográficas: <br>" + latlon_click.toString()+'<br> Fechas: <br>' + Fecha.join('<br>') )
 		.openOn(map);
 	}else if (checkBox.checked == false){ 
+		document.getElementById("historialtitle").style.display = "none";
 		circle
 		.setLatLng([0,0]);
 		}
