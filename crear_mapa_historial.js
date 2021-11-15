f886ce97b978379a23d70e738686abea8a65f031
Ykamell <90073648+Ykamell@users.noreply.github.com>
2021-11-15 00:18:59 -0500

    Update crear_mapa_historial.js

--- crear_mapa_historial.js
+++ crear_mapa_historial.js
@@ -20,6 +20,6 @@ var circle = L.circle([0,0], {
 					color: 'blue',
 					fillColor: '#69B4FD',
 					fillOpacity: 0.5,
-					radius: markerRadius(val)					
+					radius: val					
 				}).addTo(map);
 
