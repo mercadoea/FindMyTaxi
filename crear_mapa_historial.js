c990b3706bef94e71a750de317ba6ed3df07f7b1
Ykamell <90073648+Ykamell@users.noreply.github.com>
2021-11-15 00:29:36 -0500

    Update crear_mapa_historial.js

--- crear_mapa_historial.js
+++ crear_mapa_historial.js
@@ -13,13 +13,19 @@ var marker_v1f = L.marker(LatLon).addTo(map);
 var marker_v2f = L.marker(LatLon).addTo(map);
 var popup = L.popup();
 var min = 15
-function markerRadius(val) {
-  return val;
-}
+
 var circle = L.circle([0,0], {
 					color: 'blue',
 					fillColor: '#69B4FD',
 					fillOpacity: 0.5,
-					radius: val					
 				}).addTo(map);
+
+function markerRadius(val) {
+	var r = val;
+	if (r > 0) {
+        	circle.setRadius(r);
+    	} else if (diff < 0) {
+        	circle.setRadius(15);
+    	};
+}
 
