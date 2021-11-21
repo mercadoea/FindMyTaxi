ea5e28337263ca0596bd4cffe678e3a8c13d581e
Ykamell <90073648+Ykamell@users.noreply.github.com>
2021-11-21 16:32:57 -0500

    Update script_mapaHistorial.js

--- script_mapaHistorial.js
+++ script_mapaHistorial.js
@@ -16,8 +16,8 @@ function onMapClick(e) {
 			}
 		}
 		
-		for(var i = 0; i <= n - 1; i ++) {
-			document.getElementById('fechashist').innerHTML = "<b> Coordenadas geográficas: </b> <br>" + latlon_click.toString()+'<br> <b> Fechas: </b> <br>' +  i +'. ' + Fecha[i] + '<br> Distancia = ' + Distancia[i] + 'cm <br>';
+		for(var i = 0; i<= n-1; i++){
+			document.getElementById('fechashist').innerHTML = "<b> Coordenadas geográficas: </b> <br>" + latlon_click.toString()+'<br> <b> Fechas y distancias: </b> <br>' + Fecha.join(' ' + Distancia[i] + 'cm <br>') ;
 		}
 		
 		circle
